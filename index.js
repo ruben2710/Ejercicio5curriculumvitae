865de4e40fa7dd9197800476af25916b10e8fb28
ruben2710 <77392528+ruben2710@users.noreply.github.com>
2021-02-01 21:23:42 +0100

    Ejercicio5curriculumvitae

---
+++ index.js
@@ -0,0 +1,46 @@
+// Botones en la experiencia laboral, cambio de color. 
+
+const experiencialaboral = document.querySelector(".hola");
+
+function cambiocolorblue() {
+    experiencialaboral.style.backgroundColor = 'blue';
+  };
+
+  function cambiocolorwhite() {
+    experiencialaboral.style.backgroundColor = 'white';
+  };
+ 
+const experiencialaboral1 = document.querySelector(".adios");
+
+function cambiocoloryellow() {
+    experiencialaboral1.style.backgroundColor = 'yellow';
+  };
+
+function cambiocolorwhite1() {
+    experiencialaboral1.style.backgroundColor = 'white';
+  };
+
+const experiencialaboral2 = document.querySelector(".hastaluego");
+
+function cambiocolorred() {
+    experiencialaboral2.style.backgroundColor = 'red';
+  };
+
+function cambiocolorwhite2() {
+    experiencialaboral2.style.backgroundColor = 'white';
+  };
+
+
+// Botones en los idiomas, alert de cada idioma.
+
+function idiomaingles() {
+    alert('Nivel Alto. Título Superior de la Escuela Oficial de Idiomas');
+  };
+
+function idiomafrances() {
+    alert('Nivel Medio. Cursando Tercer Curso en la Escuela Oficial de Idiomas');
+  };
+
+function idiomaaleman() {
+    alert('Nivel bajo. Cursando primer Curso en la Escuela Oficial de Idiomas');
+  };
